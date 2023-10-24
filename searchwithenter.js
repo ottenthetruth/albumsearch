c165fc7de6349f374364e3c411c060990e87d955
ottenthetruth <148557557+ottenthetruth@users.noreply.github.com>
2023-10-24 15:40:34 +0200

    Bugfix V1

--- searchwithenter.js
+++ searchwithenter.js
@@ -1,4 +1,4 @@
-var searchbar1 = document.getelementById("mysearch");
+var searchbar1 = document.getElementById("mysearch");
 var searchbar2 = document.getElementById("mysearchartist");
 
 searchbar1.addEventListener("keypress", function(event) {
